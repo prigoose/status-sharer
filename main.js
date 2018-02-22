f1d600b97271082a8d00b16a6225ec9d4bfd93db
Priya <prigoose@TheHeadquarters.attlocal.net>
2018-02-21 21:18:29 -0800

    -Created a username variable -Added the username as a class in the 'user' and 'tweet' variables

--- main.js
+++ main.js
@@ -6,12 +6,36 @@ $(document).ready(function(){
   var lastNewTweets = index;
   while(index >= 0){
     var tweet = streams.home[index];
+    var username = tweet.user;
     var time = streams.home[index].created_at;
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' (' + time + ')');
+    
+    var $user = $('<span class="user"></span>');
+
+    $user.addClass(function() {
+      return username;
+    });
+
+    $user.text('@' + tweet.user + ': ');
+    $user.appendTo('.tweets');
+    
+    var $tweet = $('<div class="individualTweet"></div>');
+
+    $tweet.addClass(function() {
+      return username;
+    })
+
+    $tweet.text(tweet.message + ' (' + time + ')');
     $tweet.appendTo('.tweets');
     index -= 1;
   }
+
+// $('class=' + myClr + ']').addClass('bold');
+
+
+  $(document).on('click','.user',function(){
+    alert("hello");
+});
+
 
   $('button').on('click', function() {
     var newTweets = streams.home.length-1;
