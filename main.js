045527603ef045003c7a862feae4ca1f397f2306
Priya <prigoose@TheHeadquarters.attlocal.net>
2018-02-22 15:23:54 -0800

    -Updated program so that new tweets appear at the top of the feed, rather than the bottom -Updated program so that clicking on a user filters to their tweets only -Updated visuals for aesthetics

--- main.js
+++ main.js
@@ -3,104 +3,75 @@ $(document).ready(function(){
   // $body.html('');
 
   var index = streams.home.length - 1;
-  var lastNewTweets = index;
+  var lastNewTweet = index;
   while(index >= 0){
     var tweet = streams.home[index];
     var username = tweet.user;
     var time = streams.home[index].created_at;
+
+    var $container = $('<div class="container"></div>');    
     
     var $user = $('<span class="user"></span>');
-
-    $user.addClass(function() {
-      return username;
-    });
-
     $user.text('@' + tweet.user + ': ');
-    $user.appendTo('.tweets');
+    $user.appendTo($container);
     
     var $tweet = $('<span class="individualTweet"></span>');
-
-    $tweet.addClass(function() {
-      return username;
-    })
-
     $tweet.text(tweet.message);
-    $tweet.appendTo('.tweets');
+    $tweet.appendTo($container);
 
     var $time = $('<div class="time"></div>');
+    $time.text(time);
+    $time.appendTo($container);
 
-    $tweet.addClass(function() {
-      return username;
+    $container.addClass(function() {
+       return username;
     })
 
-    $time.text(time);
-    $time.appendTo('.tweets');
+    $container.appendTo('.tweets');
 
     index -= 1;
 
-
   }
 
-// $('class=' + myClr + ']').addClass('bold');
-
-// THIS CODE WORKS!!! GRANTED IT ONLY ADDS A  NEW CLASS, WHICH ISN'T WHAT I ULTIMATELY NEED 
-// IT TO DO. BUT IT'S A GOOD STEP
-//   $(document).on('click','.user',function(){
-//     alert("hello");
-//     $('.' + username).addClass('bold');
-// });
-
-// THIS IS THE CODE THAT'S NOT WORKING. ASK IN HELPDESK
-//   $(document).on('click','.user',function(){
-//     alert("hello");
-//     $('.user').filter('.' + username).hide();
-// });
 
 
-  $('button').on('click', function() {
-    var newTweets = streams.home.length-1;
-    while(newTweets > lastNewTweets) {
-      var tweet = streams.home[newTweets]; 
-      var username = tweet.user;
-      var time = streams.home[newTweets].created_at;
-
-      var $user = $('<span class="user"></span>');
-
-      $user.text('@' + tweet.user + ': ');
-      // APPEND TO ADDS TO END. I NEED TO ADD TO BEGINNING.
-      // BUT IF I TRY PREPEND, THEN THE TWEET TEXT SHOWS
-      // BEFORE THE USERNAME
-      $user.appendTo('.tweets');
-
-      $user.addClass(function() {
-      return username;
-      });
+  $(document).on('click','.user',function() {
+    $('.container').hide();
+    var username = $(this).closest('.container')['0'].classList['1'];
+    console.log(username);
+    $('.container').filter('.' + username).show();
+});
 
 
-      var $tweet = $('<span class="individualTweet"></span>');
+$('button').on('click', function() {
+  var index = streams.home.length-1;
+  while(lastNewTweet + 1 < index) {
+    var tweet = streams.home[index]; 
+    var username = tweet.user;
+    var time = streams.home[index].created_at;
 
-      $tweet.addClass(function() {
-      return username;
-      })
-
-      $tweet.text(tweet.message);
-      // APPEND TO ADDS TO END. I NEED TO ADD TO BEGINNING.
-      // BUT IF I TRY PREPEND, THEN THE TWEET TEXT SHOWS
-      // BEFORE THE USERNAME
-    $tweet.appendTo('.tweets');
+    var $container = $('<div class="container"></div>');    
+    
+    var $user = $('<span class="user"></span>');
+    $user.text('@' + tweet.user + ': ');
+    $user.appendTo($container);
+    
+    var $tweet = $('<span class="individualTweet"></span>');
+    $tweet.text(tweet.message);
+    $tweet.appendTo($container);
 
     var $time = $('<div class="time"></div>');
+    $time.text(time);
+    $time.appendTo($container);
 
-    $tweet.addClass(function() {
-      return username;
+    $container.addClass(function() {
+       return username;
     })
 
-    $time.text(time);
-    $time.appendTo('.tweets');
+    $container.prependTo('.tweets');
 
-    newTweets -= 1;
-    }
-    lastNewTweets = streams.home.length-1;
-  });
+    lastNewTweet += 1;
+}
+});
 
 });
