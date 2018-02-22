2e5c907acc59983ad290cf86eedf73027adcdcde
Priya <prigoose@TheHeadquarters.attlocal.net>
2018-02-21 22:39:12 -0800

    Updated the page styling to include the Twitler logo (courtesy of Noah), and varied spacing and text for readability

--- main.js
+++ main.js
@@ -18,34 +18,87 @@ $(document).ready(function(){
     $user.text('@' + tweet.user + ': ');
     $user.appendTo('.tweets');
     
-    var $tweet = $('<div class="individualTweet"></div>');
+    var $tweet = $('<span class="individualTweet"></span>');
 
     $tweet.addClass(function() {
       return username;
     })
 
-    $tweet.text(tweet.message + ' (' + time + ')');
+    $tweet.text(tweet.message);
     $tweet.appendTo('.tweets');
+
+    var $time = $('<div class="time"></div>');
+
+    $tweet.addClass(function() {
+      return username;
+    })
+
+    $time.text(time);
+    $time.appendTo('.tweets');
+
     index -= 1;
+
+
   }
 
 // $('class=' + myClr + ']').addClass('bold');
 
+// THIS CODE WORKS!!! GRANTED IT ONLY ADDS A  NEW CLASS, WHICH ISN'T WHAT I ULTIMATELY NEED 
+// IT TO DO. BUT IT'S A GOOD STEP
+//   $(document).on('click','.user',function(){
+//     alert("hello");
+//     $('.' + username).addClass('bold');
+// });
 
-  $(document).on('click','.user',function(){
-    alert("hello");
-});
+// THIS IS THE CODE THAT'S NOT WORKING. ASK IN HELPDESK
+//   $(document).on('click','.user',function(){
+//     alert("hello");
+//     $('.user').filter('.' + username).hide();
+// });
 
 
   $('button').on('click', function() {
     var newTweets = streams.home.length-1;
     while(newTweets > lastNewTweets) {
-      var tweet = streams.home[newTweets];
+      var tweet = streams.home[newTweets]; 
+      var username = tweet.user;
       var time = streams.home[newTweets].created_at;
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' (' + time + ')');
-      $tweet.prependTo('.tweets');
-      newTweets -= 1;
+
+      var $user = $('<span class="user"></span>');
+
+      $user.text('@' + tweet.user + ': ');
+      // APPEND TO ADDS TO END. I NEED TO ADD TO BEGINNING.
+      // BUT IF I TRY PREPEND, THEN THE TWEET TEXT SHOWS
+      // BEFORE THE USERNAME
+      $user.appendTo('.tweets');
+
+      $user.addClass(function() {
+      return username;
+      });
+
+
+      var $tweet = $('<span class="individualTweet"></span>');
+
+      $tweet.addClass(function() {
+      return username;
+      })
+
+      $tweet.text(tweet.message);
+      // APPEND TO ADDS TO END. I NEED TO ADD TO BEGINNING.
+      // BUT IF I TRY PREPEND, THEN THE TWEET TEXT SHOWS
+      // BEFORE THE USERNAME
+    $tweet.appendTo('.tweets');
+
+    var $time = $('<div class="time"></div>');
+
+    $tweet.addClass(function() {
+      return username;
+    })
+
+    $time.text(time);
+    $time.appendTo('.tweets');
+
+    newTweets -= 1;
     }
     lastNewTweets = streams.home.length-1;
   });
